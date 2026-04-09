acd4076ae1e90ed99e637e563bce6ce821fca7a9
soliu <solixzsystem@gmail.com>
2026-04-09 16:51:13 +0100

    baseurl updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // apiURL: 'http://102.130.121.29'
   // apiURL:'https://consmmefservices.cnsportals.com.ng'
-  apiURL:'https://apistaging.cnsportals.com.ng',
-  altUrl:'https://apistaging.cnsportals.com.ng',
-  baseUrl:'https://apistaging.cnsportals.com.ng'
+  apiURL:'https://consmmefapi.labyeducation.com',
+  altUrl:'https://consmmefapi.labyeducation.com',
+  baseUrl:'https://consmmefapi.labyeducation.com'
 };
